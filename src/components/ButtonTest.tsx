ed6e6c2dd319f4d780b9e59b03b838a49afd0b69
shelchin <shelchin@proton.me>
2024-06-30 11:55:36 +0800

    Fix typings

--- src/components/ButtonTest.tsx
+++ src/components/ButtonTest.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
 export default () => {
+  // @ts-ignore
   const { t } = useTranslation("translation");
   return <div>button test {t("hi")}</div>;
 };
