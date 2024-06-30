b188e7fa1bbd540ffa5267d64c3c9d434e4df684
shelchin <shelchin@proton.me>
2024-06-30 11:24:18 +0800

    Designing routes

---
+++ src/utils/generateStaticPaths.ts
@@ -0,0 +1,43 @@
+import type { Chain } from "viem";
+import * as chains from "viem/chains";
+
+function createCaseInsensitiveObject(obj) {
+  const lowerCaseObj = {};
+  for (let key in obj) {
+    lowerCaseObj[key.toLowerCase()] = obj[key];
+  }
+  return lowerCaseObj;
+}
+
+export const generateStaticPaths = () => {
+  const chains2 = createCaseInsensitiveObject(chains);
+  const langs = ["en", "zh"];
+  const tokentypes = ["native", "erc20", "erc721", "erc1155"];
+  const networks = Object.keys(chains2);
+  const paths = [];
+
+  for (let lang of langs) {
+    for (let tokentype of tokentypes) {
+      for (let network of networks) {
+        paths.push({ params: { network, tokentype, lang } });
+      }
+    }
+  }
+  return paths;
+};
+
+export const generateDefaultStaticPaths = () => {
+  const chains2 = createCaseInsensitiveObject(chains);
+
+  const tokentypes = ["native", "erc20", "erc721", "erc1155"];
+  const networks = Object.keys(chains2);
+  const paths = [];
+
+  for (let tokentype of tokentypes) {
+    for (let network of networks) {
+      paths.push({ params: { network, tokentype } });
+    }
+  }
+
+  return paths;
+};
